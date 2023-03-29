67b3c8c13f970913c274ed3e05602e4a0540a49d
Andreas Ernst <andreas.ernst@alvaniev.net>
2023-03-29 15:29:08 +0200

    - components HelloWorld, Counter, Carousel added

---
+++ src/views/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="container">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const msg = ref("Hello World");
+
+    return { msg };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+#container {
+  margin-left: 24px;
+}
+</style>
